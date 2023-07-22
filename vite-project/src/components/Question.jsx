d87ab8e743e74af14f6ab354323500a9299c64a8
Mark Ponomarev <you@example.com>
2023-07-22 21:15:05 +0200

    redid

--- vite-project/src/components/Question.jsx
+++ vite-project/src/components/Question.jsx
@@ -1,58 +1,22 @@
 import React from "react";
 import { useState, useEffect } from "react";
 export default function Question(props) {
-  const btnCheckedStyle = {
-    border: "none",
-  };
-
-  const parentId = props.id();
-  let btnName = 'question-btn'
-  const btns = props.answers.map((e) => {
-    
-    return (
-      <button
-        correctanswer={e === props.correctAnswer ? e : false}
-        parentid={parentId}
-        id={props.id()}
-        key={props.id()}
-        onClick={(event) => props.answerClicked(event, parentId)}
-        className={btnName}
-      >
-        {e}
-      </button>
-    );
-  })
-  useEffect(() => {
-   
-  },[props.checking])
-  
-  // .map(btn => {
-  //   console.log(btn.props.className)
-  //   return btn
-  // });
-  
+ 
 
 
-  // if(props.checking){
-
-  //   for (let btn of btns) {
-  //     console.log('pog',btnName);
-  //     if (btn.props.className.includes("choosen")) {
-  //       console.log(btn);
-  //       if (btn.props.correctanswer) {
-  //         btnName += "right";
-  //       } else {
-  //         btnName += "wrong";
-  //       }
-  //     } else {
-  //       btnName += "unchoosen";
-  //     }
-  //   }
-  // }
+const btns = props.questions.answers.map(btn => {
 
   return (
-    <div id={parentId} className="question">
-      <p className="question-title">{props.question}</p>
+    <button choosen={btn.choosen} onClick={(event) => props.answerClicked(event)} className="question-btn" key={btn.id} id={btn.id} >
+      {btn.answer}
+    </button>
+  )
+})
+
+
+  return (
+    <div  className="question">
+      <p className="question-title">{props.questions.question}</p>
       <div className="qustion-btns">{btns}</div>
     </div>
   );
--- vite-project/src/pages/Quiz.jsx
+++ vite-project/src/pages/Quiz.jsx
@@ -2,107 +2,51 @@ import { useState, useEffect } from "react";
 import { decode } from "html-entities";
 import uniqid from "uniqid";
 import Question from "../components/Question";
-
+// .map(e => decode(e, { level: "html5" }) )
 export default function Quiz() {
-  const [questions, setQuestion] = useState("");
-  const [answerBtns, setAnswerBtns] = useState("");
-  const [questionsData, setQuestionData] = useState();
-  const [checking, setChecking] = useState(false);
-  const [btnName, setBtnName] = useState('question-btn');
-  const [currentBtn, setCurrentBtn] = useState();
-
+  const [questions, setQuestions] = useState();
+  const [answers, setAnswers] = useState();
   useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=5")
       .then((res) => res.json())
-      .then((data) => {
-        setQuestionData(() => data);
-        setQuestion(
-          data.results.map((e) => ({
-            ...e,
-            answers: [...e.incorrect_answers, e.correct_answer],
-           
-          }))
-        );
-      });
-  }, []);
-console.log(questions[0].answers)
-//PUT THEM IN OBJECTS
-
-
-  // .map((e) => ({
-  //   question: decode(e.question, { level: "html5" }),
-  //   answers: decode(e.answers, { level: "html5" }),
-  // }))
-
-function checkAnswers(){
-  const btns = document.querySelectorAll('.question-btn')
-  for (let btn of btns) {
-      if (btn.props.className.includes("choosen")) {
-        if (btn.props.correctanswer) {
-          setBtnName(oldVal => oldVal += "right") ;
-        } else {
-          setBtnName(oldVal => oldVal += "wrong") ;
-        }
-      } else {
-        setBtnName(oldVal => oldVal  += "unchoosen");
-      }
-    }
-}
-  function answerClicked(e,current) {
-    setCurrentBtn(current)
-    console.log(questions)
-    console.log(this,e.target)
-    // console.log(e.target.attributes.parentid.value)
-    // e.target.classList.toggle("choosen");
-    // const otherBtns = e.target
-    //   .closest(".qustion-btns")
-    //   .querySelectorAll(".question-btn");
-    // for (let btn of otherBtns) {
-    //   if (btn !== e.target) {
-    //     btn.classList.remove("choosen");
-    //   }
-    // }
-  }
-  const questionsDOM =
-    questions.length !== 0 &&
-    questions.map((e) => {
-      return (
-        <Question
-       
-        btnName={btnName}
-        setCurrentBtn={setCurrentBtn}
-        setBtnName={setBtnName}
-          setChecking={setChecking}
-          checking={checking}
-           checkAnswers={checkAnswers}
-          correctAnswer={e.correct_answer}
-          key={uniqid()}
-          answerClicked={answerClicked}
-          id={uniqid}
-          question={decode(e.question, { level: "html5" })}
-          answers={e.answers.map((a) => decode(a, { level: "html5" }))}
-        />
+      .then((data) =>
+        setQuestions(
+          data.results.map((item) => {
+            return {
+              ...item,
+              question:decode(item.question, { level: "html5" }),
+              answers: [item.correct_answer, ...item.incorrect_answers].map(
+                (e) => {
+                  return {
+                    answer: decode(e, { level: "html5" }),
+                    id: uniqid(),
+                    choosen: false,
+                  };
+                }
+              ),
+            };
+          })
+        )
       );
-    });
+  }, []);
+  // question && console.log(...question.map(e => e))
+
+ const qestionDOM = questions?.map((question,i) => {
+  console.log(questions[i])
+  // const [id,choosen,answer] = e.answers.map(answer => {})
+  function answerClicked(event){
+  //  setQuestions(oldVal => {
+  //    return {...oldVal,answers:oldVal[i].answers.map(e => { return e.id === event.target.id ? {...e,choosen:!e.choosen} : e })}
+  //   })
+   
+  }
+    return <Question answerClicked={answerClicked} key={uniqid()}  questions={question} setQuestions={setQuestions} index={i}  />;
+  });
   return (
-    <main>
-      <div className="quiz">
-        <div className="quiz-con">
-          {questionsDOM}
-          {/* <div className="question">
-        <p className="question-title">How would one say goodbye in Spanish?</p>
-        <div className="qustion-btns">
-        <button className="question-btn">Adiós</button>
-        <button className="question-btn">Hola</button>
-        <button className="question-btn">Au Revoir</button>
-        <button className="question-btn">Salir</button>
-        </div>  
-      </div>  */}
-        </div>
-        <button onClick={() => checkAnswers()} className="btn answers-btn">
-          Check answers
-        </button>
+    <div className="quiz">
+      <div className="quiz-con">
+    {qestionDOM}
       </div>
-    </main>
+    </div>
   );
 }
